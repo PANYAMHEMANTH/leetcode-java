154a2e6ec67989989d129c906a7289efa6fe995b
Hemanth <133361769+PANYAMHEMANTH@users.noreply.github.com>
2026-01-31 15:21:52 +0530

    Time: 9 ms (23.73%) | Memory: 44.6 MB (38.78%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,18 +1,7 @@
-import java.util.*;
-
 class Solution {
     public String reverseWords(String s) {
-        // Split by whitespace, filter out empty strings, reverse, join with space
-        String[] words = s.trim().split("\\s+");
-        
-        StringBuilder result = new StringBuilder();
-        for (int i = words.length - 1; i >= 0; i--) {
-            if (i < words.length - 1) {
-                result.append(" ");
-            }
-            result.append(words[i]);
-        }
-        
-        return result.toString();
+        List<String> words = Arrays.asList(s.trim().split("\\s+"));
+        Collections.reverse(words);
+        return String.join(" ", words);
     }
 }
